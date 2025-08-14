19dbfa8e0236fc064604a40289c10d4e4bc8d6a0
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-14 23:15:12 +0600

    added provider and dynamodb for Database table

---
+++ day-04-healthtech-api/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# DynamoDB Table
+resource "aws_dynamodb_table" "users" {
+  name         = "healthtech-users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "HealthTech Users Table"
+  }
+}
